ec4077f85fb7f67cab773deaa620a1eff965d7e3
Jakub Hozak <jakub.hozak@gmail.com>
2010-08-17 12:27:19 +0200

    personalizations

---
+++ jakub.el
@@ -0,0 +1,52 @@
+;; (require 'yasnippet)
+;; (yas/initialize)
+;; (yas/load-directory "~/.emacs.d/jakub/snippets")
+;; (setq yas/prompt-functions '(yas/ido-prompt yas/dropdown-prompt yas/no-prompt yas/x-prompt))
+
+
+; Copy & Paste
+(setq mouse-drag-copy-region nil)  ; stops selection with a mouse being immediately injected to the kill ring
+(setq x-select-enable-primary nil)  ; stops killing/yanking interacting with primary X11 selection
+(setq x-select-enable-clipboard t)  ; makes killing/yanking interact with clipboard X11 selection
+(setq select-active-regions t) ;  active region sets primary X11 selection
+(global-set-key [mouse-2] 'mouse-yank-primary)  ; make mouse middle-click only paste from primary X11 selection, not clipboard and kill ring.
+(setq yank-pop-change-selection t)  ; makes rotating the kill ring change the X11 clipboard.
+
+; Fixing Dired
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+  (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
+
+
+; Fixing defaults
+(setq require-final-newline nil)
+(define-key global-map "\C-h" 'backward-delete-char)
+(define-key global-map "\C-t" 'help-command)
+
+(setq my-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+(set-frame-font my-font)
+
+
+
+(defun open-utility-file (name)
+                        (find-file-other-frame name)
+                        (set-frame-font my-font)
+                        (toggle-truncate-lines)
+)
+
+(global-set-key [f10] (lambda () (interactive) (open-utility-file  "~/.emacs.d/snippets/text-mode/ruby-mode/")))
+(global-set-key [f11] (lambda () (interactive) (open-utility-file  "~/.emacs.d/jakub.el")))
+(global-set-key [f12] (lambda () (interactive) (open-utility-file  "~/.bashrc")))
+
+
+
+
